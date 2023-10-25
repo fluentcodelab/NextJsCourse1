e850f77727194b5837c0be057dcbf119045fda5c
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-25 01:57:50 -0400

    Data fetching

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,6 +1,18 @@
-const UserPage = () => {
+interface User {
+    id: number;
+    name: string;
+}
+
+const UserPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users: User[] = await res.json()
     return (
-        <div>UserPage</div>
+        <>
+            <h1>Users</h1>
+            <ul>
+                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>
     )
 }
 
