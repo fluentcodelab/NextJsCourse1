8c0628b910bceff1976309565967928a33d6726d
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-28 17:27:22 -0400

    Programmatic navigation

--- next-app/app/users/new/page.tsx
+++ next-app/app/users/new/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+// Since this will be a client action, we transform as a client component
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const NewUserPage = () => {
-    return (
-        <div>NewUserPage</div>
-    )
-}
-export default NewUserPage
+  const router = useRouter();
+  return (
+    <button className="btn btn-primary" onClick={() => router.push('/users')}>
+      Create
+    </button>
+  );
+};
+export default NewUserPage;
--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,4 +1,5 @@
 import UserTable from '@/app/users/UserTable';
+import Link from 'next/link';
 
 interface Props {
   searchParams: { sortOrder: string };
@@ -11,6 +12,9 @@ const UserPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
       <UserTable sortOrder={sortOrder} />
     </>
   );
